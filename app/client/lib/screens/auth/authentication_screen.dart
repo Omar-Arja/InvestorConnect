d93f776e5d7d8dc958abbe8f08c4e72545038cd8
Omar-Arja <omaralarja17@gmail.com>
2023-09-10 18:29:14 +0300

    [Client] Created authorization screen

---
+++ app/client/lib/screens/auth/authentication_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:client/widgets/custom_buttons.dart';
+import 'package:client/widgets/or_divider.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'Investor',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Connect',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(right: 34),
+                  child: Text(
+                    'Unlocking Opportunities, One Connection at a Time.',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Color.fromARGB(255, 96, 96, 96),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Image.asset(
+                  'assets/images/auth.jpg',
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 25),
+                GoogleButton(onPressed: () {}),
+                const SizedBox(height: 20),
+                const OrDivider(),
+                const SizedBox(height: 20),
+                CustomButton(
+                  onPressed: () {},
+                  text: 'Log In',
+                ),
+                const SizedBox(height: 20),
+                CustomButton(
+                  onPressed: () {},
+                  text: 'Sign Up',
+                ),
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
