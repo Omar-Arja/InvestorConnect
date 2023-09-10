65082c3a752f33de5af6f23c92c6ffab0ebb3f87
Omar-Arja <omaralarja17@gmail.com>
2023-09-10 18:28:02 +0300

    [Client] Added routes & configs in main.dart

--- app/client/lib/main.dart
+++ app/client/lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:client/screens/auth/authentication_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(App(prefs));
+}
+
+class App extends StatelessWidget {
+  final SharedPreferences prefs;
+
+  const App(this.prefs, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        primaryColor: const Color.fromARGB(255, 76, 104, 175), 
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'InvestorConnect',
+      initialRoute: '/auth',
+      routes: {
+        '/auth': (context) => const AuthScreen(),
+      },
+    );
+  }
 }
