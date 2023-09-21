1b53d9a2ebf9a803dbbbb06127063f9cb5f71ab4
Omar-Arja <omaralarja17@gmail.com>
2023-09-21 17:49:23 +0300

    [Client] Created api service for startup profile creation

--- app/client/lib/services/api_service.dart
+++ app/client/lib/services/api_service.dart
@@ -1,9 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:client/models/startup_profile.dart';
+import 'package:dio/dio.dart';
+import 'package:http_parser/http_parser.dart';
 
 class ApiService {
   static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static Map<String, String> headers = {
+    'Authorization': 'Bearer ',
+  };
 
+  // Auth requests
   static Future<Map<String, dynamic>> login(String email, String password) async {
     final url = Uri.parse('$baseUrl/auth/login');
 
@@ -14,6 +21,8 @@ class ApiService {
       });
 
       if (response.statusCode == 200) {
+        headers['Authorization'] = 'Bearer ${json.decode(response.body)['authorisation']['token']}';
+
         return json.decode(response.body);
       } else {
         throw Exception('Login failed');
@@ -41,6 +50,40 @@ class ApiService {
       }
     } catch (e) {
       throw Exception('Signup failed: $e');
+    }
+  }
+
+  // Startup profile requests
+  static Future<Map<String, dynamic>> createStartupProfile(StartupProfileModel startupData) async {
+
+    const url = '$baseUrl/startup/create-profile';
+    final dio = Dio();
+    dio.options.headers['Authorization'] = '${headers['Authorization']}';
+    dio.options.headers['Content-Type'] = 'multipart/form-data';
+    
+    final FormData formData = FormData.fromMap({
+      'company_name': startupData.companyName,
+      'location': startupData.location,
+      'industries': startupData.industries.join(','),
+      'investment_stage': startupData.investmentStage,
+      'company_description': startupData.companyDescription,
+      'min_investment_amount': startupData.minInvestmentAmount.toString(),
+      'max_investment_amount': startupData.maxInvestmentAmount.toString(),
+      'preferred_locations': startupData.preferredLocations.join(','),
+      'company_logo_file': await MultipartFile.fromFile(startupData.logoFile!.path, contentType: MediaType('image', startupData.logoFile!.path.split('.').last)),
+      'pitch_video_file': await MultipartFile.fromFile(startupData.pitchVideoFile!.path, contentType: MediaType('video', startupData.pitchVideoFile!.path.split('.').last)),
+    });
+
+    try {
+      final response = await dio.post(url, data: formData);
+
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        return response.data;
+      } else {
+          return {'status': 'error', 'message': 'An error occurred'};
+      }
+    } catch (e) {
+      return {'status': 'error', 'message': 'An unexpected error occurred'};
     }
   }
 }
