f499186828ae18795898f092e57c6e604787b512
Omar-Arja <omaralarja17@gmail.com>
2023-09-26 23:22:41 +0300

    [Client] Created Profile model

---
+++ app/client/lib/models/profile.dart
@@ -0,0 +1,11 @@
+class Profile {
+  final String name;
+  final String aiAnalysis;
+  final String pictureUrl;
+
+  Profile({
+    required this.name,
+    required this.aiAnalysis,
+    required this.pictureUrl,
+  });
+}
