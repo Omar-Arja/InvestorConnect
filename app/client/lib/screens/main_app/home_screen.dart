8a96b361cb50d340ba962cb275e5e752385b5391
Omar-Arja <omaralarja17@gmail.com>
2023-09-26 23:28:05 +0300

    [Client] Created home screen widget

--- app/client/lib/screens/main_app/home_screen.dart
+++ app/client/lib/screens/main_app/home_screen.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:client/models/profile.dart';
+import 'package:client/models/investor_profile.dart';
+import 'package:client/models/startup_profile.dart';
 import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:client/widgets/ui/profile_card.dart';
 import 'package:client/widgets/ui/home_buttons.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  final List<InvestorProfileModel>? investorProfiles;
+  final List<StartupProfileModel>? startupProfiles;
 
+  const HomeScreen({super.key , this.investorProfiles, this.startupProfiles});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final AppinioSwiperController _swiperController = AppinioSwiperController();
+  Profile? currentProfile;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.investorProfiles?.isNotEmpty == true) {
+      currentProfile = Profile(
+        name: widget.investorProfiles![0].fullName,
+        aiAnalysis: widget.investorProfiles![0].aiAnalysis,
+        pictureUrl: widget.investorProfiles![0].profilePictureUrl,
+      );
+    } else if (widget.startupProfiles?.isNotEmpty == true) {
+      currentProfile = Profile(
+        name: widget.startupProfiles![0].companyName,
+        aiAnalysis: widget.startupProfiles![0].aiAnalysis,
+        pictureUrl: widget.startupProfiles![0].companyLogoUrl,
+      );
+    }
+  }
+
+  void setCurrentProfile(bool isInvestor, int index) {
+    if (isInvestor && index < widget.investorProfiles!.length && widget.investorProfiles?.isNotEmpty == true) {
+      Profile newProfile = Profile(
+      name: widget.investorProfiles![index].fullName,
+      aiAnalysis: widget.investorProfiles![index].aiAnalysis,
+      pictureUrl: widget.investorProfiles![index].profilePictureUrl,
+    );
+    setState(() {
+      currentProfile = newProfile;
+    });
+    } else if (!isInvestor && index < widget.startupProfiles!.length && widget.startupProfiles?.isNotEmpty == true) {
+      Profile newProfile = Profile(
+      name: widget.startupProfiles![index].companyName,
+      aiAnalysis: widget.startupProfiles![index].aiAnalysis,
+      pictureUrl: widget.startupProfiles![index].companyLogoUrl,
+    );
+    setState(() {
+      currentProfile = newProfile;
+    });
+    }
+  }
 
   void swipeLeft() {
     _swiperController.swipeLeft();
@@ -19,6 +71,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isInvestor = widget.investorProfiles != null;
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -60,24 +114,37 @@ class HomeScreen extends StatelessWidget {
           maxHeight: MediaQuery.of(context).size.height,
         ),
         padding: const EdgeInsets.fromLTRB(24, 0, 24, 90),
-        child: Column(
+        child: widget.investorProfiles?.isNotEmpty == true || widget.startupProfiles?.isNotEmpty == true ?
+        Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
               child: AppinioSwiper(
                 padding: const EdgeInsets.only(bottom: 17),
-                cardsCount: 10,
-                loop: true,
+                cardsCount: isInvestor ? widget.investorProfiles!.length : widget.startupProfiles!.length,
                 cardsSpacing: 40,
                 backgroundCardsCount: 1,
                 controller: _swiperController,
-                cardsBuilder: (context, index) => ProfileCard(),
+                onSwipe: (index, direction) {
+                  setCurrentProfile(isInvestor, index);
+                },
+                cardsBuilder: (context, index) {
+                  if (isInvestor && index < widget.investorProfiles!.length) {
+                    return ProfileCard(investorProfile: widget.investorProfiles![index],);
+                  }
+                  else if (!isInvestor && index < widget.startupProfiles!.length) {
+                    return ProfileCard(startupProfile: widget.startupProfiles![index],);
+                  }
+                  else {
+                    return Container();
+                  }
+                },
               ),
             ),
-            HomeButtons(leftButton: swipeLeft, rightButton: swipeRight),
+            HomeButtons(leftButton: swipeLeft, rightButton: swipeRight, currentProfile: currentProfile),
           ],
-        ),
+        ) : const Center(child: Text('No Profiles Found')),
       ),
     );
   }
