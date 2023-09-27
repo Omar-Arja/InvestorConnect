f397e44de1358ffdb572e9c4620a4189772ab753
Omar-Arja <omaralarja17@gmail.com>
2023-09-27 23:45:01 +0300

    [Client] Added id property to Profile model

--- app/client/lib/models/profile.dart
+++ app/client/lib/models/profile.dart
@@ -1,9 +1,11 @@
 class Profile {
+  int? id;
   final String name;
   final String aiAnalysis;
   final String pictureUrl;
 
   Profile({
+    this.id,
     required this.name,
     required this.aiAnalysis,
     required this.pictureUrl,
