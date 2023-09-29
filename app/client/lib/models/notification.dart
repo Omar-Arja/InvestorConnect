9af254fb13f99915c3e9dee37786f3f15c2cb120
Omar-Arja <omaralarja17@gmail.com>
2023-09-29 19:36:20 +0300

    [Client] Updated notification model naming

--- app/client/lib/models/notification.dart
+++ app/client/lib/models/notification.dart
@@ -1,19 +1,19 @@
 import 'package:intl/intl.dart';
 
-class Notification {
+class NotificationModel {
   final String title;
   final String body;
   final DateTime createdAt;
   final String time;
 
-  Notification({
+  NotificationModel({
     required this.title,
     required this.body,
     required this.createdAt,
   }) : time = DateFormat.jm().format(createdAt);
 
-  factory Notification.fromJson(Map<String, dynamic> json) {
-    return Notification(
+  factory NotificationModel.fromJson(Map<String, dynamic> json) {
+    return NotificationModel(
       title: json['title'] ?? '',
       body: json['body'] ?? '',
       createdAt: DateTime.parse(json['created_at'] ?? ''),
