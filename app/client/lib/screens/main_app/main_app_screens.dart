ea2ced81c258741653cfa55fe11bd3c75351524d
Omar-Arja <omaralarja17@gmail.com>
2023-09-30 01:42:21 +0300

    [Client] Refactored code & improved performance

--- app/client/lib/screens/main_app/main_app_screens.dart
+++ app/client/lib/screens/main_app/main_app_screens.dart
@@ -1,11 +1,12 @@
 import 'package:client/models/investor_profile.dart';
+import 'package:client/models/notification.dart';
 import 'package:client/models/startup_profile.dart';
+import 'package:client/models/user_profile.dart';
 import 'package:client/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:client/widgets/ui/bottom_navbar.dart';
 import 'package:client/screens/main_app/home_screen.dart';
 import 'package:client/screens/main_app/messages/chats_screen.dart';
-import 'package:client/screens/main_app/calendar_screen.dart';
 import 'package:client/screens/main_app/notifications_screen.dart';
 import 'package:client/screens/main_app/profile/profile_screen.dart';
 
@@ -18,39 +19,99 @@ class MainAppScreens extends StatefulWidget {
 
 class _MainAppScreensState extends State<MainAppScreens> {
   int _currentIndex = 0;
-  List<StartupProfileModel> startupProfiles = [];
   List<InvestorProfileModel> investorProfiles = [];
+  List<StartupProfileModel> startupProfiles = [];
+  List<NotificationModel> notifications = [];
+  List<UserProfile> profiles = [];
+  bool isDataLoaded = false;
 
   @override
   void initState() {
     super.initState();
-    getPotentialMatches();
+    getAllData();
+  }
+
+  Future<void> getAllData() async {
+    Future.wait([
+      getPotentialMatches(),
+      getChats(),
+      getNotifications(),
+    ]);
   }
   
-  void getPotentialMatches() async {
+  Future<void> getPotentialMatches() async {
+    final List<StartupProfileModel> newStartupProfiles = [];
+    final List<InvestorProfileModel> newInvestorProfiles = [];
     final response = await ApiService.getPotentialMatches();
+    
     if (response['status'] == 'success') {
-      final List potentialMatches = response['potential_matches'];
+      final potentialMatches = response['potential_matches'];
       for (final match in potentialMatches) {
         if (response['usertype_name'] == 'investor') {
           final investorProfile = InvestorProfileModel.fromJson(match);
-          investorProfiles.add(investorProfile);
-
+          newInvestorProfiles.add(investorProfile);
         } else if (response['usertype_name'] == 'startup') {
           final startupProfile = StartupProfileModel.fromJson(match);
-          startupProfiles.add(startupProfile);
+          newStartupProfiles.add(startupProfile);
         }
-      }
-        
+      } 
+
       setState(() {
-        investorProfiles;
-        startupProfiles;
+        investorProfiles = newInvestorProfiles;
+        startupProfiles = newStartupProfiles;
+        isDataLoaded = true;
+      });
+    }
+  }
+
+
+  Future<void> getChats() async {
+    final List<UserProfile> newProfiles = [];
+    final response = await ApiService.getChats();
+
+    if (response['status'] == 'success') {
+      final List chatsData = response['profiles'];
+      for (final chat in chatsData) {
+        final profile = UserProfile.fromJson(chat);
+        newProfiles.add(profile);
+      }
+      newProfiles.sort((a, b) => b.messages.last.createdAt.compareTo(a.messages.last.createdAt));
+      setState(() {
+        profiles = newProfiles;
+      });
+    }
+  }
+
+  Future<void> getNotifications() async {
+    final List<NotificationModel> newNotifications = [];
+    final response = await ApiService.getNotifications();
+    
+    if (response['status'] == 'success') {
+      final List notificationsData = response['notifications'];
+      for (final notification in notificationsData) {
+        final notificationModel = NotificationModel.fromJson(notification);
+        newNotifications.add(notificationModel);
+      }
+      newNotifications.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+      setState(() {
+        notifications = newNotifications;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+
+    if (!isDataLoaded) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Color.fromARGB(255, 61, 78, 129),
+            semanticsLabel: 'Loading',
+          ),
+        ),
+      );
+    }
 
     return Scaffold(
       body: _buildScreen(_currentIndex),
@@ -69,48 +130,15 @@ class _MainAppScreensState extends State<MainAppScreens> {
   Widget _buildScreen(int index) {
     switch (index) {
       case 0:
-        if (investorProfiles.isNotEmpty || startupProfiles.isNotEmpty) {
-          return HomeScreen(investorProfiles: investorProfiles, startupProfiles: startupProfiles);
-        } else {
-          return PotentialMatchesLoader(investorProfiles: investorProfiles, startupProfiles: startupProfiles);
-        }
+        return HomeScreen(investorProfiles: investorProfiles, startupProfiles: startupProfiles, updateMatches: getPotentialMatches);
       case 1:
-        return const ChatsScreen();
+        return ChatsScreen(profiles: profiles, updateChats: getChats);
       case 2:
-        return const CalendarScreen();
+        return NotificationsScreen(notifications: notifications, updateNotifications: getNotifications);
       case 3:
-        return const NotificationsScreen();
-      case 4:
         return const ProfileScreen();
       default:
-        return HomeScreen(investorProfiles: investorProfiles, startupProfiles: startupProfiles);
+        return HomeScreen(investorProfiles: investorProfiles, startupProfiles: startupProfiles, updateMatches: getPotentialMatches);
     }
-  }
-}
-
-class PotentialMatchesLoader extends StatelessWidget {
-  const PotentialMatchesLoader({
-    super.key,
-    required this.investorProfiles,
-    required this.startupProfiles,
-  });
-
-  final List<InvestorProfileModel> investorProfiles;
-  final List<StartupProfileModel> startupProfiles;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: ApiService.getPotentialMatches(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return HomeScreen(investorProfiles: investorProfiles, startupProfiles: startupProfiles);
-          
-        } else if (snapshot.hasError) {
-          return const Center(child: Text('Error loading potential matches'));
-        }
-        return const Center(child: CircularProgressIndicator());
-      },
-    );
   }
 }
