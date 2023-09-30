1bc96b908c784b3159452bfd7301dc894b689c0d
Omar-Arja <omaralarja17@gmail.com>
2023-09-30 13:12:18 +0300

    [Client] Created profile screen

--- app/client/lib/screens/main_app/profile_screen.dart
+++ app/client/lib/screens/main_app/profile_screen.dart
@@ -1,8 +1,11 @@
+import 'package:client/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  final Map<String, dynamic> profile;
+
+  const ProfileScreen({super.key, required this.profile});
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +24,94 @@ class ProfileScreen extends StatelessWidget {
             fontSize: 24,
           ),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Color.fromARGB(255, 61, 78, 129),
+            ),
+            onPressed: () async {
+              await ApiService.logout();
+              Navigator.pushNamedAndRemoveUntil(context, '/auth', (route) => false);
+            },
+          ),
+        ],
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: Colors.white,
-      body: const Center(
-        child: Text('Welcome to the Profile Screen!'),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundColor: const Color.fromARGB(255, 61, 78, 129),
+                  backgroundImage: NetworkImage(profile['profilePictureUrl']),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                profile['fullName'],
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                profile['location'],
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  profile['description'],
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16, 
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await ApiService.logout();
+                    Navigator.pushNamedAndRemoveUntil(context, '/auth', (route) => false);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 61, 78, 129),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
