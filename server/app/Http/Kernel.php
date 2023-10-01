a5ac4ef04e1ce7bd09527240adec993bb922fb6d
Omar-Arja <omaralarja17@gmail.com>
2023-10-01 21:29:19 +0300

    [Server] Created admin check middleware

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth.api' => \App\Http\Middleware\AuthenticationMiddleware::class,
+        'auth.admin' => \App\Http\Middleware\AdminMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
