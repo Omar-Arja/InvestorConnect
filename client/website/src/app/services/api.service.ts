52bb9565afb10704a94fb2bea6fe084eff95c6e3
Omar-Arja <omaralarja17@gmail.com>
2023-10-01 21:21:41 +0300

    [Client] Created api service class

---
+++ client/website/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) { }
+  
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/auth/login`, { email, password });
+  }
+}
