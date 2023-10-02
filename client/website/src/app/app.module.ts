585c79198ec8a1b1c7895de44b5d2d452febdd7e
Omar-Arja <omaralarja17@gmail.com>
2023-10-02 03:08:51 +0300

    [Client] Added routes

--- client/website/src/app/app.module.ts
+++ client/website/src/app/app.module.ts
@@ -3,23 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgChartsModule } from 'ng2-charts';
 import { FormsModule } from '@angular/forms';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DonutChartComponent } from './components/donut-chart/donut-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, SidebarComponent, DashboardComponent, DonutChartComponent, BarChartComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NgChartsModule,
     FormsModule,
+    CanvasJSAngularChartsModule,
     MatCardModule,
     MatInputModule,
     MatButtonModule,
